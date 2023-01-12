55faa07c063faab9b64f6b8912b6307e617a7e30
Ben Cavins <ben.cavins@flatironschool.com>
2023-01-12 16:50:47 -0500

    implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const taskForm = document.querySelector('#create-task-form')
+
+    taskForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const descField = document.querySelector('#new-task-description')
+        // console.log(descField.value)
+
+        const taskList = document.querySelector('#tasks')
+        const newTask = document.createElement('li')
+        newTask.textContent = descField.value
+
+        const priority = document.querySelector('#priority')
+        // console.log(priority)
+        console.log(priority.value)
+
+        if (priority.value === 'High') {
+          newTask.classList.add('red')
+        } else if (priority.value === 'Med') {
+          newTask.classList.add('yellow')
+        } else {
+          newTask.classList.add('green')
+        }
+        
+        
+        const delBtn = document.createElement('button')
+        delBtn.textContent = 'Delete'
+        delBtn.addEventListener('click', (event) => {
+            // console.log('you clicked')
+            newTask.remove()
+        })
+
+
+        newTask.append(delBtn)
+        taskList.append(newTask)
+    })
 });
